9060404b917e3da56f2f2451cfdbce9d565b5c27
jumma <aarifhingorja4@gmail.com>
2023-06-19 16:11:04 +0530

    first commit

---
+++ src/components/CloseModal.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "./ui/Button";
+import { useRouter } from "next/navigation";
+
+const CloseModal = () => {
+  const router = useRouter();
+  const handleClick = () => router.back();
+  return (
+    <Button
+      onClick={handleClick}
+      variant={"subtle"}
+      className="h-6 w-6 p-0 rounded-md"
+      aria-label="close modal"
+    >
+      <X className="h-4 w-4" />
+    </Button>
+  );
+};
+
+export default CloseModal;
---
+++ src/types/redis.d.ts
@@ -0,0 +1,12 @@
+import { Vote, VoteType } from "@prisma/client";
+
+
+export type CachedPost =  {
+    id: string;
+    title: string;
+    authorUsername: string;
+    content: string
+    currentVote: VoteType | null;
+    createdAt: Date
+
+}
